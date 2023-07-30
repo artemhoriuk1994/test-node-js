e39ea00173ff238a53093c40806c5c56552778bd
Artem Horiuk <62579471+artemhoriuk1994@users.noreply.github.com>
2023-07-30 14:11:21 +0000

    add conditions for cheking of correct answer condition

--- app/quiz/quiz.js
+++ app/quiz/quiz.js
@@ -1,7 +1,7 @@
 const { stdin, stdout } = require('process');
 const questions = require('./questions')
 const readline = require('readline');
-console.log(questions)
+
 const rl = readline.createInterface({
     input: stdin,
     output: stdout
@@ -9,23 +9,39 @@ const rl = readline.createInterface({
 
 let score = 0;
 let currentQuestion = 0;
+const wrongConditionAnswers = [];
+
+function askOneMoreTime (questionId) {
+  const wrongCondition = questions.find(id => questionId !== id);
+  console.log(wrongCondition)
+  wrongConditionAnswers.push(wrongCondition)
+  if (!wrongConditionAnswers) return;
+  console.log(wrongConditionAnswers)
+}
 
 function askQuestion() {
   if (currentQuestion < questions.length) {
-    const { question, options, correctAnswer } = questions[currentQuestion];
+    const { question, options, correctAnswer, id } = questions[currentQuestion];
 
-    console.log(`Питання №${currentQuestion + 1}: ${question}`);
+    console.log(`Питання №${id}: ${question}`);
     options.forEach((option, index) => {
       console.log(`${index + 1}. ${option}`);
     });
 
     rl.question('Виберіть варіант відповіді (1-3): ', (answer) => {
-      const selectedOption = options[parseInt(answer) - 1];
-      if (selectedOption === correctAnswer) {
+      const selectedOption = parseInt(answer)
+      const optionsForSelections = [1,2,3]
+      if (optionsForSelections.includes(selectedOption)) {
+        if(options[selectedOption -1] === correctAnswer){
         score++;
-      }
+    }
       currentQuestion++;
       askQuestion();
+      } else {
+        console.log('Будь ласка виберыть варіант 1, 2 або 3')
+        askQuestion()
+      }
+      
     });
   } else {
     console.log(`Гра завершена! Ваш результат: ${score} правильних відповідей із ${questions.length} питань.`);
